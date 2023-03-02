5f96bf89dd150646f33a76eddb2336ce30117499
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2023-03-02 18:30:51 +0100

    this app works

--- app/src/main/java/com/example/chatjet/ui/screen/ChatFragment.kt
+++ app/src/main/java/com/example/chatjet/ui/screen/ChatFragment.kt
@@ -9,13 +9,11 @@ import com.example.chatjet.R
 import com.example.chatjet.base.BaseFragment
 import com.example.chatjet.data.model.Chat
 import com.example.chatjet.data.model.ChatGroup
-import com.example.chatjet.data.model.User
 import com.example.chatjet.services.FirebaseRepository
 import com.example.chatjet.ui.adapter.ChatAdapter
 import com.example.chatjet.view_model.MainViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.*
-import com.google.firebase.firestore.EventListener
 import kotlinx.android.synthetic.main.fragment_chat.*
 import java.util.*
 import kotlin.collections.ArrayList
@@ -26,25 +24,26 @@ class ChatFragment : BaseFragment() {
     private var chatList = ArrayList<Chat>()
     private lateinit var adapter: ChatAdapter
 
+//    private var limit = 25
+
     private val db = FirebaseFirestore.getInstance()
     private val fbAuth = FirebaseAuth.getInstance()
 
     private val currentUserUid: String?
         get() = fbAuth.currentUser?.uid
 
-    private val dbUser = db.collection("users").document(currentUserUid!!)
     private val dbChat = db.collection("chat")
 
     // Added object which registration listener in Firebase
     private var chatListenerRegistration: ListenerRegistration? = null
 
     private val viewModel: MainViewModel by activityViewModels()
+    val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
 
     override fun subscribeUi() {
 
         val userUid = requireArguments().getString("uid").toString()
 
-        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         // Wczytanie elementów w recycler view od dołu:
         layoutManager.stackFromEnd = true
         chatRecyclerView.layoutManager = layoutManager
@@ -52,10 +51,20 @@ class ChatFragment : BaseFragment() {
 
         viewModel.fetchFullNameUser(userUid, requireView())
 
-        returnBT.setOnClickListener {
+//        chatRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+//            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+//                val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+//                if (firstVisibleItemPosition == 0) {
+//                    readMessage(currentUserUid!!, userUid, limit)
+//                    limit += 25 // Zwiększ limit o kolejne 25 wiadomości przy każdym wczytywaniu
+//                }
+//            }
+//        })
 
+        returnBT.setOnClickListener {
             // Exit from listener when you click button return
-            chatListenerRegistration!!.remove()
+            chatListenerRegistration?.remove()
             findNavController().navigate(R.id.action_chatFragment_to_usersFragment)
         }
 
@@ -69,40 +78,7 @@ class ChatFragment : BaseFragment() {
                 sendMessage(FirebaseRepository().currentUserUid!!, userUid, message)
                 Log.d("REPOUSER", "$userUid, $message")
                 writeMessage.setText("")
-//                dbUser.addSnapshotListener(object : EventListener<DocumentSnapshot> {
-//                    override fun onEvent(
-//                        snapshot: DocumentSnapshot?,
-//                        error: FirebaseFirestoreException?
-//                    ) {
-//                        if (error != null) {
-//                            Log.w("TAG", "Listen failed.", error)
-//                            return
-//                        }
-//                        if (snapshot != null && snapshot.exists()) {
-//                            val user = snapshot.toObject(User::class.java)
-//                            val namee: String = user!!.full_name.toString()
-//
-//                            sendMessage(FirebaseRepository().currentUserUid!!, userUid, message)
-//                            Log.d("REPOUSER", "$userUid, $message")
-//                            writeMessage.setText("")
-//                            topic = "/topics/${userUid}"
-//                            PushNotification(
-//                                NotificationData("${namee} :", message),
-//                                topic
-//                            ).also {
-//                                sendNotification(it)
-//                            }
-//
-//                            db.collection("user").document(userUid)
-//                                .collection("last_message")
-//                                .document(firebaseUser.uid)
-//                                .update("message", message)
 
-//                        } else {
-//                            Log.d("TAG", "Current data: null")
-//                        }
-//                    }
-//                })
             }
         }
         readMessage(currentUserUid!!, userUid)
@@ -116,63 +92,65 @@ class ChatFragment : BaseFragment() {
 
     }
 
-    private fun readMessage(senderId: String, receiverId: String) {
+    private fun readMessage(senderId: String, receiverId: String, limit: Int = chatList.size) {
 
         // Implemented object listener which is listening change in Firebase
-        chatListenerRegistration = dbChat.addSnapshotListener { snapshot, error ->
-            if (error != null) {
-                Log.w("TAG", "Listen failed.", error)
-                return@addSnapshotListener
-            }
+        chatListenerRegistration = dbChat
+            .orderBy("timestamp", Query.Direction.DESCENDING)
+//            .limit(limit.toLong())
+            .addSnapshotListener { snapshot, error ->
+                if (error != null) {
+                    Log.w("TAG", "Listen failed.", error)
+                    return@addSnapshotListener
+                }
 
-            if (snapshot != null && !snapshot.isEmpty) {
-                chatList.clear()
+                if (snapshot != null && !snapshot.isEmpty) {
+                        chatList.clear()
 
-                for (document in snapshot.documents) {
-                    val chat = document.toObject(Chat::class.java)
+                    for (document in snapshot.documents) {
+                        val chat = document.toObject(Chat::class.java)
 
-                    if (chat!!.senderId == senderId && chat.receiverId == receiverId ||
-                        chat.senderId == receiverId && chat.receiverId == senderId
-                    ) {
-                        chatList.add(chat)
+                        if (chat!!.senderId == senderId && chat.receiverId == receiverId ||
+                            chat.senderId == receiverId && chat.receiverId == senderId
+                        ) {
+                            chatList.add(chat)
+                        }
                     }
+
+                    // Sort the chatList by timestamp in descending order
+                    chatList.sortByDescending { it.timestamp }
+
+                    // If database has more than 1 message
+                    if (chatList.size > 1) {
+                        // Reverse the chatList to display the latest messages at the bottom
+                        chatList = chatList.reversed() as ArrayList<Chat>
+                    }
+
+                    // Group the chatList by date
+                    val groupedChatList =
+                        chatList.groupBy { it.sentAt?.let { it1 -> getDateString(it1) } }
+
+                    // Create a new list to display chat groups
+                    val chatGroupList = mutableListOf<ChatGroup>()
+
+                    // Iterate through the groupedChatList and create ChatGroup objects
+                    for ((date, chats) in groupedChatList) {
+                        chatGroupList.add(ChatGroup(date.toString(), chats))
+                    }
+
+                    adapter = ChatAdapter(chatList)
+                    chatRecyclerView.adapter = adapter
+                    Log.d("REPOADAPTER", "$adapter")
+
+                } else {
+
+                    // List is empty, load empty layout
+                    chatList.clear()
+                    adapter = ChatAdapter(chatList)
+                    chatRecyclerView.adapter = adapter
+
                 }
-
-                // Sort the chatList by timestamp in descending order
-                chatList.sortByDescending { it.timestamp }
-
-                // If database has more than 1 message
-                if (chatList.size > 1) {
-                    // Reverse the chatList to display the latest messages at the bottom
-                    chatList = chatList.reversed() as ArrayList<Chat>
-                }
-
-                // Group the chatList by date
-                val groupedChatList =
-                    chatList.groupBy { it.sentAt?.let { it1 -> getDateString(it1) } }
-
-                // Create a new list to display chat groups
-                val chatGroupList = mutableListOf<ChatGroup>()
-
-                // Iterate through the groupedChatList and create ChatGroup objects
-                for ((date, chats) in groupedChatList) {
-                    chatGroupList.add(ChatGroup(date.toString(), chats))
-                }
-
-                adapter = ChatAdapter(chatList)
-                chatRecyclerView.adapter = adapter
-                Log.d("REPOADAPTER", "$adapter")
-
-
-            } else {
-
-                // List is empty, load empty layout
-                chatList.clear()
-                adapter = ChatAdapter(chatList)
-                chatRecyclerView.adapter = adapter
-
             }
-        }
     }
 
     private fun getDateString(date: Date): String {
