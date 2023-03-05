e3e368396c3d5bd890f59563b9ddd280f261c415
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2023-03-05 17:20:20 +0100

    update 1.0.3

--- app/src/main/java/com/example/chatjet/view_model/MainViewModel.kt
+++ app/src/main/java/com/example/chatjet/view_model/MainViewModel.kt
@@ -1,13 +1,13 @@
 package com.example.chatjet.view_model
 
+import android.content.Context
 import android.view.View
 import android.widget.TextView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.chatjet.R
-import com.example.chatjet.data.model.Chat
 import com.example.chatjet.data.model.User
-import com.example.chatjet.services.FirebaseRepository
+import com.example.chatjet.services.s.repository.FirebaseRepository
 
 class MainViewModel: ViewModel() {
 
@@ -21,10 +21,17 @@ class MainViewModel: ViewModel() {
         }
     }
 
-    fun fetchFullNameUser(userUid: String, v: View){
+    fun fetchFullNameUser(userUid: String, v: View, context: Context){
         repository.fetchFullNameUser(userUid) {
             val fullName = v.findViewById<TextView>(R.id.nameUser)
+//            val photo = v.findViewById<ImageView>(R.id.photo)
             fullName.text = it?.full_name
+
+//            Glide.with(context)
+//                .load(it?.photo)
+//                .override(120, 120)
+//                .circleCrop()
+//                .into(photo)
         }
     }
 }
