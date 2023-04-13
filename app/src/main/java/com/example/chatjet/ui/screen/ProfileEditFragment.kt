37030a7b56b1965db45cafd8840219980efa7f30
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2023-04-13 21:10:46 +0200

    update 2.0.2

---
+++ app/src/main/java/com/example/chatjet/ui/screen/ProfileEditFragment.kt
@@ -0,0 +1,16 @@
+package com.example.chatjet.ui.screen
+
+import com.example.chatjet.R
+import com.example.chatjet.base.BaseFragment
+
+class ProfileEditFragment : BaseFragment() {
+    override val layout: Int = R.layout.fragment_profile_edit
+
+    override fun subscribeUi() {
+
+    }
+
+    override fun unsubscribeUi() {
+
+    }
+}
--- app/src/main/java/com/example/chatjet/ui/screen/ProfileFragment.kt
+++ app/src/main/java/com/example/chatjet/ui/screen/ProfileFragment.kt
@@ -1,12 +1,18 @@
 package com.example.chatjet.ui.screen
 
+import androidx.navigation.fragment.findNavController
 import com.example.chatjet.R
 import com.example.chatjet.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment: BaseFragment() {
     override val layout: Int = R.layout.fragment_profile
 
     override fun subscribeUi() {
+
+        editProfileButton.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_profileEditFragment)
+        }
 
     }
 
