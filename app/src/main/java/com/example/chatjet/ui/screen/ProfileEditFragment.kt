5d8cb98a99c1f10b5a53a7f27d8867c22c09fb45
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2023-05-08 23:26:13 +0200

    added new buttons

--- app/src/main/java/com/example/chatjet/ui/screen/ProfileEditFragment.kt
+++ app/src/main/java/com/example/chatjet/ui/screen/ProfileEditFragment.kt
@@ -1,12 +1,26 @@
 package com.example.chatjet.ui.screen
 
+import android.view.View
 import com.example.chatjet.R
 import com.example.chatjet.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_profile_edit.*
 
 class ProfileEditFragment : BaseFragment() {
     override val layout: Int = R.layout.fragment_profile_edit
 
     override fun subscribeUi() {
+
+        checkNumberButton.setOnClickListener {
+
+            confirmNumber.visibility = View.VISIBLE
+            confirmNumberView.visibility = View.VISIBLE
+        }
+
+        acceptStatusButton.setOnClickListener {
+
+            acceptedStatusButton.visibility = View.VISIBLE
+            acceptStatusButton.visibility = View.GONE
+        }
 
     }
 
